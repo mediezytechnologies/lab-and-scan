99ef6608fce5ee612141d2a4e38672db873f6731
mediezytechnologies <mediezytech@gmail.com>
2024-06-11 16:46:14 +0530

    dfsdsf

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,21 +8,15 @@ import Foundation
 import device_info_plus
 import file_selector_macos
 import path_provider_foundation
-<<<<<<< Updated upstream
-=======
 import sqflite
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
->>>>>>> Stashed changes
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-<<<<<<< Updated upstream
-=======
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
->>>>>>> Stashed changes
 }

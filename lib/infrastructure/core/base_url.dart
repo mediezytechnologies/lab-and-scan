e2449702b2e4bcad51f12d9dce542b5138629fa4
Akber A A <165126011+mediezytechnologies@users.noreply.github.com>
2024-06-22 18:04:44 +0530

    change to main server

--- lib/infrastructure/core/base_url.dart
+++ lib/infrastructure/core/base_url.dart
@@ -1,2 +1,6 @@
-const loginBaseUrl = "https://test.mediezy.com/api";
-const baseUrl = "https://test.mediezy.com/api/lab";
+// const loginBaseUrl = "https://test.mediezy.com/api";
+// const baseUrl = "https://test.mediezy.com/api/lab";
+
+
+const loginBaseUrl = "https://mediezy.com/api";
+const baseUrl = "https://mediezy.com/api/lab";
